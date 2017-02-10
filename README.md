6b07e4b6c7596b6a6d7c132ed2037f750ad881a6
hanguangchao <hanguangchao@gmail.com>
2017-02-11 03:05:29 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Yii 实战
+
+内容索引
+* [feature_api_module](https://github.com/hanguangchao/artisanyii/tree/feature_api_module) 实现接口模块统一JSON输出
---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=artisanyii',
+    'username' => 'homestead',
+    'password' => 'secret',
+    'charset' => 'utf8',
+];
